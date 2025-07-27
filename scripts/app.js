87cd8c714b45450efc97a7fa85013f107bb860bd
Max <maksimscernovs07@gmail.com>
2025-07-27 18:36:57 +0300

    Optimization

--- scripts/app.js
+++ scripts/app.js
@@ -18,34 +18,3 @@ burgerMenu.addEventListener('click', menu =>{
 
 
 
-function globalGradient() {
-  const interBubble = document.querySelector('.interactive')
-
-  let curX = 0,
-      curY = 0,
-      tgX = 0,
-      tgY = 0
-
-  function move() {
-    curX += (tgX - curX) / 20
-    curY += (tgY - curY) / 20
-
-    gsap.set(interBubble, {
-      x: Math.round(curX),
-      y: Math.round(curY)
-    })
-
-    requestAnimationFrame(() => {
-      move()
-    })
-  }
-
-  window.addEventListener('mousemove', (event) => {
-  tgX = event.clientX
-  tgY = event.clientY
-})
-
-  move()
-}
-
-globalGradient()
