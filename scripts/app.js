70f7adf943e89c76740da1d938aa24dff483b550
Max <maksimscernovs07@gmail.com>
2025-08-03 09:33:37 +0300

    Home page

--- scripts/app.js
+++ scripts/app.js
@@ -1,54 +1,4 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-
-if (ScrollTrigger.isTouch !== 1) {
-
-	ScrollSmoother.create({
-		wrapper: '.wrapper',
-		content: '.content',
-		smooth: 1.5,
-		effects: true
-	})
-
-	gsap.fromTo('.hero-section', { opacity: 1 }, {
-		opacity: 0,
-		scrollTrigger: {
-			trigger: '.hero-section',
-			start: 'center',
-			end: '820',
-			scrub: true
-		}
-	})
-
-	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item .but-cont' )
-
-	itemsL.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: -50 }, {
-			opacity: 1, x: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-850',
-				end: '-100',
-				scrub: true
-			}
-		})
-	})
-
-	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
-
-	itemsR.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: 50 }, {
-			opacity: 1, x: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-750',
-				end: 'top',
-				scrub: true
-			}
-		})
-	})
-
-}
-
+import './scroll.js'
 
 const burgerMenu = document.querySelector('.menu-button__menu')
 if (burgerMenu) {
@@ -62,6 +12,7 @@ burgerMenu.addEventListener('click', menu =>{
   if (burgerMenu.classList.contains('active')) {
     menuBackground.classList.add('active')
     mainMenu.classList.add('active')
+    
   }else{
     menuBackground.classList.remove('active')
     mainMenu.classList.remove('active')
