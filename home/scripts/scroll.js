5b2676633ebb1735e89dbc337fd26fd8a14098fa
Max <maksimscernovs07@gmail.com>
2025-08-08 20:10:02 +0300

    .

--- home/scripts/scroll.js
+++ home/scripts/scroll.js
@@ -1,55 +1,63 @@
 console.log('scroll.js loaded')
 
-
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
+// Проверка — ПК или тач-устройство
 if (ScrollTrigger.isTouch !== 1) {
 
-	ScrollSmoother.create({
-		wrapper: '.wrapper',
-		content: '.content',
-		smooth: 1.5,
-		effects: true,
-		
-	})
+    // ПК: плавный скролл
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true
+    })
 
-	gsap.fromTo('.hero-section', { opacity: 1 }, {
-		opacity: 0,
-		scrollTrigger: {
-			trigger: '.hero-section',
-			start: 'center',
-			end: '820',
-			scrub: true
-		}
-	})
+} else {
 
-	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item .but-cont' )
+    // Мобила: заводской скролл
+    document.querySelector('.wrapper').style.overflow = 'visible';
+    document.querySelector('.content').style.transform = 'none';
 
-	itemsL.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: -50 }, {
-			opacity: 1, x: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-850',
-				end: '-100',
-				scrub: true
-			}
-		})
-	})
-
-	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
-
-	itemsR.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: 50 }, {
-			opacity: 1, x: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-750',
-				end: 'top',
-				scrub: true
-			}
-		})
-	})
-
+    // Важно — сбрасываем GSAP transform для совместимости
+    gsap.set('.content', { clearProps: 'transform' });
 }
 
+// Анимации будут работать везде
+gsap.fromTo('.hero-section', { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.hero-section',
+        start: 'center',
+        end: '820',
+        scrub: true
+    }
+})
+
+let itemsL = gsap.utils.toArray('.gallery__left .gallery__item .but-cont')
+
+itemsL.forEach(item => {
+    gsap.fromTo(item, { opacity: 0, x: -50 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-100',
+            scrub: true
+        }
+    })
+})
+
+let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+itemsR.forEach(item => {
+    gsap.fromTo(item, { opacity: 0, x: 50 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-750',
+            end: 'top',
+            scrub: true
+        }
+    })
+})
