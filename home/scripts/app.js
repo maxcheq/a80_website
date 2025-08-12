49fc82ed7b7332e8cb1b4ffad8c33520ad409091
Max <maksimscernovs07@gmail.com>
2025-08-12 22:38:24 +0300

    Adding contact menu

--- home/scripts/app.js
+++ home/scripts/app.js
@@ -17,9 +17,42 @@ burgerMenu.addEventListener('click', menu =>{
     menuBackground.classList.remove('active')
     mainMenu.classList.remove('active')
   }
-})}
+})
+menuBackground.addEventListener('click', (e) => {
+    if (e.target === menuBackground) {
+        menuBackground.classList.remove('active')
+        mainMenu.classList.remove('active')
+        burgerMenu.classList.remove('active') 
+    }
+  })
+}
 
 
+const contactButton = document.querySelector('.contact-button')
+if (contactButton) {
+  const contactButtonCancel = document.querySelector('.button_cancel')
+  const contactBackground = document.querySelector('.contact__background')
+  const contactContainer = document.querySelector('.contact__container')
 
+  
+  contactButton.addEventListener('click', () => {
+    contactBackground.classList.add('active')
+    contactContainer.classList.add('active')
+  })
+
+ 
+  contactButtonCancel.addEventListener('click', () => {
+    contactBackground.classList.remove('active')
+    contactContainer.classList.remove('active')
+  })
+
+
+  contactBackground.addEventListener('click', (e) => {
+    if (e.target === contactBackground) {
+      contactBackground.classList.remove('active')
+      contactContainer.classList.remove('active')
+    }
+  })
+}
 
 
